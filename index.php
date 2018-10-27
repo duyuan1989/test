03c739de43a78d0f723a2a8036674aa6d8e01e72
root <root@localhost.localdomain>
2018-10-27 09:15:18 +0000

    this is test my composer pockagist

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+	require_once 'vendor/autoload.php';
+	 $test = new duyuan\test\Duyuan();
+	$test->run();
---
+++ src/Duyuan.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace duyuan\test;
+
+class Duyuan{
+
+
+	public function run(){
+
+		echo 'composer is run ....';
+	}
+}
